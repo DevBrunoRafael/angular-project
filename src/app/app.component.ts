33d5410f9908ee7fb1ee76c48d1251cfb18a5e73
Bruno Rafael <brunorafaeldls2016@gmail.com>
2024-05-16 09:16:28 -0300

    aula sobre pipes e diretivas ngIf ngSwitch e ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-project';
+
+  numeroGerado?: number;
+  readonly Status = Status;
+  currentStatus: Status = Status.CANCELADO;
+
+  handleClick(numero: number) {
+    this.numeroGerado = numero;
+  }
+
+  changeStatus(event: Event) {
+    const value = (event.target as HTMLSelectElement).value;
+    this.currentStatus = value as Status;
+  }
+
+  clearNumber() {
+    this.numeroGerado = undefined;
+  }
+}
+
+export enum Status {
+  PENDENTE = "PENDENTE",
+  CANCELADO = "CANCELADO",
+  ENTREGUE = "ENTREGUE"
 }
--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,23 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   template: `
-    <div class="m-10 bg-[#ccc] h-[30rem] p-6 rounded-md flex items-center justify-around">
-      <input type="text" name="name" class="block border border-gray-400">
-
-      <div class="flex gap-2">
-        <button class="bg-black text-white rounded-md px-3 py-1.5">
-          Button
-        </button>
-
-        <button class="bg-black text-white rounded-md px-3 py-1.5">
-          Button
-        </button>
-      </div>
-    </div>
+    <button class="bg-black text-white rounded-md px-3 py-1.5" [ngClass]="twClass" (click)="generateNumber()">
+      {{ text }}
+    </button>
   `,
 })
 export class ButtonComponent {
+  @Input() text: string = '';
+  @Input() twClass: string = '';
+  @Output() onClick: EventEmitter<number> = new EventEmitter<number>();
 
+  generateNumber() {
+    const number = Math.floor(Math.random() * 100);
+    this.onClick.emit(number);
+  }
 }
