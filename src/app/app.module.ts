40ab55bd4d355f01301a331955c223d26c2cd037
Bruno Rafael <brunorafaeldls2016@gmail.com>
2024-05-15 21:27:38 -0300

    add and config tailwindcss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ButtonComponent } from './components/button/button.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
